96a390185dba8e59c7af94a7305e914a0ce115a4
Ayush Jain <ayushjain21.nmims@gmail.com>
2021-11-26 00:35:37 +0530

    Assignment of axios

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const cowinController= require("../controllers/cowinController")
+const weatherController= require("../controllers/weatherController")
 
-router.get("/cowin/states", cowinController.getStatesList)
-router.get("/cowin/districts/:stateId", cowinController.getDistrictsList)
-router.get("/cowin/centers", cowinController.getByPin)
-router.post("/cowin/getOtp", cowinController.getOtp)
+router.get("/getWeather",weatherController.getLondonWeather)
+router.get("/getLondonTemp",weatherController.londonTemp)
+router.get("/sortingCities",weatherController.sortCities)
 
 
 module.exports = router;
